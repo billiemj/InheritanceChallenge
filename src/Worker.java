e28289a529807a95fea3229a91c3aa56e5ada54d
Billie <billiemj@gmail.com>
2023-03-24 06:51:34 -0500

    add

---
+++ src/Worker.java
@@ -0,0 +1,16 @@
+public class Worker {
+    private String name;
+    private String birthDate;
+    private String endDate;
+
+    public Worker(String name, String birthDate, String endDate) {
+        this.name = name;
+        this.birthDate = birthDate;
+        this.endDate = endDate;
+    }
+
+    public void getAge(int age){
+
+
+    }
+}
