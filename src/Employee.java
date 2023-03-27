6776a76a066d8e9a706e4cab01e7a9fae1f96f47
Billie <billiemj@gmail.com>
2023-03-27 06:57:14 -0500

    add

--- src/Employee.java
+++ src/Employee.java
@@ -2,4 +2,21 @@ public class Employee extends Worker{
     private long employeeId;
     private String hireDate;
 
+
+    //Static fields let you share data across all instances
+    private static int employeeNo = 1;
+
+    public Employee(String name, String birthDate, String hireDate) {
+        super(name, birthDate);
+        this.employeeId = Employee.employeeNo++;
+        this.hireDate = hireDate;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "employeeId=" + employeeId +
+                ", hireDate='" + hireDate + '\'' +
+                "} " + super.toString();
+    }
 }
